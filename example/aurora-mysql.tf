c21b4d56bcef668283bef3d185cef8b3afedec15
Zafarraza <112979784+Zafarraza@users.noreply.github.com>
2025-03-14 20:59:31 +1300

    Update aurora-mysql.tf

--- example/aurora-mysql.tf
+++ example/aurora-mysql.tf
@@ -11,14 +11,14 @@ data "aws_db_cluster_snapshot" "test_snapshot" {
 module "aws_rds_cluster_from_snapshot" {
   source = "../modules/aurora-mysql"
 
+  rds_auto_minor_version_upgrade = local.rds_auto_minor_version_upgrade
+  rds_cluster_database_name      = local.name
   rds_cluster_engine             = local.rds_cluster_engine
   rds_cluster_engine_version     = local.rds_cluster_engine_version
-  rds_cluster_database_name      = local.name
-  rds_cluster_master_username    = local.rds_cluster_master_username
   rds_cluster_instance_class     = local.rds_cluster_instance_class
-  rds_multi_az                   = local.rds_multi_az
-  rds_auto_minor_version_upgrade = local.rds_auto_minor_version_upgrade
+  rds_cluster_master_username    = local.rds_cluster_master_username
   rds_cluster_vpc_id             = local.rds_cluster_vpc_id
+  rds_multi_az                   = local.rds_multi_az
   rds_skip_final_snapshot        = local.rds_skip_final_snapshot
   rds_subnet_ids                 = local.rds_subnet_ids
   rds_snapshot_identifier        = data.aws_db_cluster_snapshot.test_snapshot.id
