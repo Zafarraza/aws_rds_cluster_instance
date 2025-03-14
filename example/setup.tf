6c8c19d861e6e468d4f1ed9ed670160871ebf91b
Zafarraza <112979784+Zafarraza@users.noreply.github.com>
2025-03-14 21:10:53 +1300

    Refactor/ct-0001 sort alphabetically
    * Refactor aurora-mysql.tf
    
    * Refactor setup.tf
    
    * Refactor main.tf

--- example/setup.tf
+++ example/setup.tf
@@ -4,14 +4,14 @@ terraform {
 }
 
 locals {
+  rds_auto_minor_version_upgrade = true
   rds_cluster_engine             = "aurora-mysql"
   rds_cluster_engine_version     = "8.0.mysql_aurora.3.05.2"
   rds_cluster_instance_class     = "db.t4g.medium"
   rds_cluster_master_username    = ""
-  rds_subnet_ids                 = ["subnet-", "subnet-", "subnet-"]
-  rds_multi_az                   = true
-  rds_auto_minor_version_upgrade = true
   rds_cluster_vpc_id             = "vpc-"
+  rds_multi_az                   = true
+  rds_subnet_ids                 = ["subnet-", "subnet-", "subnet-"]
   rds_skip_final_snapshot        = true
   tags = {
     "Business:Backup"         = false
